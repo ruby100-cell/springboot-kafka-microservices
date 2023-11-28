82c51243693e15b6eb734ffa753d4b934600282b
Ruby Gupta <rubybtechbanasthali@gmail.com>
2023-11-28 19:07:08 +0530

    Order, Stock And Email Service using Apache kafka

---
+++ order-service/src/main/java/net/javaguides/orderservice/controller/OrderController.java
@@ -0,0 +1,37 @@
+package net.javaguides.orderservice.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.basedomains.dto.Order;
+import net.javaguides.basedomains.dto.OrderEvent;
+import net.javaguides.orderservice.kafka.OrderProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+	
+	@Autowired
+	private OrderProducer orderProducer;
+	
+	@PostMapping("/orders")
+	public String placeOrder(@RequestBody Order order) {
+		
+		order.setOrderId(UUID.randomUUID().toString());
+		
+		OrderEvent orderEvent = new OrderEvent();
+		orderEvent.setStatus("PENDING");
+		orderEvent.setMessage("order status is in pending state");
+		orderEvent.setOrder(order);
+		
+		orderProducer.sendMessage(orderEvent);
+		
+		return "Order placed successfully !!";
+	}
+
+}
